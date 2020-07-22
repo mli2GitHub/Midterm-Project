dafac12a71c67752a605a7729b7cf82dc7920f0b
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-07-22 13:23:16 -0400

    so change the code

---
+++ code.py
@@ -0,0 +1,30 @@
+points = 0
+
+print("This is a Would - You - Rather - like game. Choose a letter and see if your choice was popular or unpopular.")
+
+def input_(prompt,ans1,ans2):
+    global points
+    while True:
+        input__=input(prompt).lower()
+        if input__ == ans1:
+            print("Popular!")
+            points += 1
+            break
+        elif inputt == ans2:
+            print("Unpopular")
+            break
+        else:
+            print("...")
+
+input_("Cats (c) or Dogs (d)? ", "d", "c")
+
+input_("Toilet Paper Under (u) or Toilet Paper Over (o)? ", "o", "u")
+
+input_("Wii (W) or Xbox (X)? ", "w", "x")
+
+if points == 3:
+  input("You are totally in the mainstream!")
+elif points == 2:
+  input("You've got one outlier, but that's completely fine!")
+else:
+  input("You wanted to avoid the mainstream, huh?")
